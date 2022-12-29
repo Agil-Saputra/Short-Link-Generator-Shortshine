ac2879ca516a7f7eb9499e52d3cce6eae91d5bfa
Agil <88566690+Agil-Saputra@users.noreply.github.com>
2022-12-29 08:24:05 +0700

    initial commit

---
+++ main.js
@@ -0,0 +1,90 @@
+// burger menu on mobile only
+const navmenu = document.querySelector(".nav-menu");
+const burger = document.querySelector(".menu");
+
+burger.addEventListener("click", () => {
+  navmenu.classList.toggle("active");
+  burger.classList.toggle("close");
+});
+
+// IntersectionObserver for page animation
+const observer = new IntersectionObserver((entry) => {
+  entry.forEach((element) => {
+    if (element.isIntersecting) {
+      element.target.classList.add("show");
+    } else {
+      element.target.classList.remove("show");
+    }
+  });
+});
+
+const hide = document.querySelectorAll(".hidden");
+hide.forEach((el) => observer.observe(el));
+
+// variables for fetching data
+const form = document.querySelector("#form");
+const input = document.querySelector("#url");
+const container = document.querySelector("#container");
+
+// fetch API for Shorting The input URl Using Asynchronous function
+async function short() {
+  const data = await fetch(
+    `https://api.shrtco.de/v2/shorten?url=${input.value}`
+  );
+  const res = await data.json();
+
+  if (res.ok == true) {
+    let div = document.createElement("div");
+    div.classList.add("child");
+    div.innerHTML += `
+    <h2>Here is your link:<a href="${res.result.original_link}" target="_blank"><span id="link">${res.result.short_link3}</span></a></h2>
+    <button id="copy">
+            <img src="public/images/copy2.svg" title="copy">
+            Copy
+          </button>
+    `;
+    container.appendChild(div);
+  } else {
+    const err = document.querySelector(".error");
+    err.style.display = "block";
+    err.innerHTML = `
+    <h2>${res.error}. Because your link is ${res.disallowed_reason}</h2>
+    `;
+    setTimeout(() => {
+      err.style.display = "none";
+    }, 10000);
+  }
+  // function to copy url to clipboard
+  const copy = document.querySelectorAll("#copy");
+  
+  copy.forEach((el) => {
+    el.addEventListener("click", () => {
+      const link = document.querySelector("#link");
+      navigator.clipboard.writeText(link.innerText);
+      el.innerText = "Copied!";
+    });
+  });
+}
+
+// add event listener to submit button
+const loader = document.querySelector(".loader");
+const child = document.querySelector(".child");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (!input.value) {
+    const errtext = document.querySelector(".error-text");
+    errtext.innerHTML = `Please Enter a your valid input!`;
+    errtext.style.display = "block";
+    setTimeout(() => {
+      errtext.style.display = "none";
+    }, 2000);
+  } else {
+    short();
+    loader.style.display = "block";
+    setTimeout(() => {
+      loader.style.display = "none";
+    }, 1500);
+  }
+  input.value = " ";
+});
